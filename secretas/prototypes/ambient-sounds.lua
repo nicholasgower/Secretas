fe3484b763c4d00013383b6bc4ed6eeef8137b54
Nicholas Gower <nicholasgower01@gmail.com>
2025-07-30 12:26:15 -0700

    sound: Corrected incorrect sound reference in Factorio 2.0.61.

--- secretas/prototypes/ambient-sounds.lua
+++ secretas/prototypes/ambient-sounds.lua
@@ -73,10 +73,8 @@ data:extend(
 -------------------------------------------------------------------------------------SPACE
 
 })
---[[
-local hero_sound = data.raw["ambient-sound"]["frozeta-4-hero"]
 
-if helpers.compare_versions(helpers.game_version,"2.0.59") > -1 then
+local hero_sound = data.raw["ambient-sound"]["frozeta-4-hero"]
+if helpers.compare_versions(helpers.game_version,"2.0.60") > -1 then
   hero_sound.sound = "__space-age__/sound/ambient/aquilo/aquilo-3-hero.ogg"
 end
---]]
